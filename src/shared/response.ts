c10d5386382f50c135e16f6044cea37d01958f82
RuslanKostiuk <ruslankostiuk@gmail.com>
2018-12-31 12:59:05 +0200

    removed console log

--- src/shared/response.ts
+++ src/shared/response.ts
@@ -5,7 +5,6 @@ export type Response = {
 
 export class ResponseBuider {
     public static BuildResponse(body?: any, status: number = 200): Response {
-        if (body && body[0]) console.log(body);
         return{
             body: body,
             status: status
